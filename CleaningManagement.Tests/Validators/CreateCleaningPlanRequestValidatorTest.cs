31fa1f86e889ce9f080eb06574226861b8374622
AndreiYarashenka <andrei.yarashenka@innowise-group.com>
2022-03-23 16:17:22 +0300

    #2 Tests for validators

---
+++ CleaningManagement.Tests/Validators/CreateCleaningPlanRequestValidatorTest.cs
@@ -0,0 +1,144 @@
+using CleaningManagement.Api.Models;
+using CleaningManagement.Api.Validators;
+using NUnit.Framework;
+
+namespace CleaningManagement.Tests.Validators
+{
+    public class CreateCleaningPlanRequestValidatorTest
+    {
+        private readonly CreateCleaningPlanRequestValidator _validator = new();
+        
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("       ")]
+        public void Title_WithEmptyOrWhiteSpaceValue_ShouldBeIsNotValid(string title)
+        {
+            // Arrange
+            var request = new CreateCleaningPlanRequest(title, 1, "Description");
+            
+            // Act
+            var validationResult = _validator.Validate(request);
+            
+            // Assert
+            Assert.IsFalse(validationResult.IsValid);
+        }
+        
+        [Test]
+        public void Title_WithLengthMoreThanMaxLength_ShouldBeIsNotValid()
+        {
+            // Arrange
+            var maxLength = 256;
+            var title = "title".PadRight(maxLength + 1);
+            var request = new CreateCleaningPlanRequest(title, 1, "Description");
+            
+            // Act
+            var validationResult = _validator.Validate(request);
+            
+            // Assert
+            Assert.IsFalse(validationResult.IsValid);
+        }
+        
+        [Test]
+        [TestCase("title")]
+        [TestCase("correct test")]
+        [TestCase("text")]
+        public void Title_NotEmptyWithValidLength_ShouldBeIsValid(string title)
+        {
+            // Arrange
+            var request = new CreateCleaningPlanRequest(title, 1, "Description");
+            
+            // Act
+            var validationResult = _validator.Validate(request);
+            
+            // Assert
+            Assert.IsTrue(validationResult.IsValid);
+        }
+        
+        [Test]
+        public void CustomerId_LessThanZero_ShouldBeIsNotValid()
+        {
+            // Arrange
+            var request = new CreateCleaningPlanRequest("Title", -1, "Description");
+            
+            // Act
+            var validationResult = _validator.Validate(request);
+            
+            // Assert
+            Assert.IsFalse(validationResult.IsValid);
+        }
+        
+        [Test]
+        public void CustomerId_EqualZero_ShouldBeIsNotValid()
+        {
+            // Arrange
+            var request = new CreateCleaningPlanRequest("Title", 0, "Description");
+            
+            // Act
+            var validationResult = _validator.Validate(request);
+            
+            // Assert
+            Assert.IsFalse(validationResult.IsValid);
+        }
+        
+        [Test]
+        public void CustomerId_MoreThanZero_ShouldBeIsValid()
+        {
+            // Arrange
+            var request = new CreateCleaningPlanRequest("Title", 1, "Description");
+            
+            // Act
+            var validationResult = _validator.Validate(request);
+            
+            // Assert
+            Assert.IsTrue(validationResult.IsValid);
+        }
+        
+        [Test]
+        [TestCase("")]
+        [TestCase(null)]
+        [TestCase("       ")]
+        public void Description_WithEmptyOrWhiteSpaceValue_ShouldBeIsValid(string description)
+        {
+            // Arrange
+            var request = new CreateCleaningPlanRequest("Title", 1, description);
+            
+            // Act
+            var validationResult = _validator.Validate(request);
+            
+            // Assert
+            Assert.IsTrue(validationResult.IsValid);
+        }
+        
+        [Test]
+        public void Description_WithLengthMoreThanMaxLength_ShouldBeIsNotValid()
+        {
+            // Arrange
+            var maxLength = 512;
+            var description = "description".PadRight(maxLength + 1);
+            var request = new CreateCleaningPlanRequest("Title", 1, description);
+            
+            // Act
+            var validationResult = _validator.Validate(request);
+            
+            // Assert
+            Assert.IsFalse(validationResult.IsValid);
+        }
+        
+        [Test]
+        [TestCase("Description")]
+        [TestCase("correct test")]
+        [TestCase("text")]
+        public void Description_NotEmptyWithValidLength_ShouldBeIsValid(string description)
+        {
+            // Arrange
+            var request = new CreateCleaningPlanRequest("Title", 1, description);
+            
+            // Act
+            var validationResult = _validator.Validate(request);
+            
+            // Assert
+            Assert.IsTrue(validationResult.IsValid);
+        }
+    }
+}
